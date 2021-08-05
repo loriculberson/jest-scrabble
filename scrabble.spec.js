4bfc98d28a8645818bcef1aab776e38796936ffb
Lori Culberson <bigskybandit@sbcglobal.net>
2021-08-05 18:45:56 -0400

    test skeleton

---
+++ scrabble.spec.js
@@ -0,0 +1,18 @@
+//test the 'scrabble' module
+describe('scrabble', () => {
+  //test the method '#scoreWord'
+  describe('#scoreWord', () => {
+    //provide a condition
+    describe('given valid word', () => {
+      //describe what the method should do for this condition
+      it('returns the sum of the tile values for the word', () => {
+          
+      })
+    })
+    
+    describe('given invalid word', () => {
+    
+
+    })
+  })
+})
